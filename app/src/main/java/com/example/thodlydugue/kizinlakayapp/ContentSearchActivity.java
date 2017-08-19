b938e051b65961a86b6986e1f60a1bb366f4ea4d
Jimmy Joseph <dioucyalewemenn@yahoo.fr>
2017-08-19 14:55:54 -0700

    nothing new

--- app/src/main/java/com/example/thodlydugue/kizinlakayapp/ContentSearchActivity.java
+++ app/src/main/java/com/example/thodlydugue/kizinlakayapp/ContentSearchActivity.java
@@ -3,12 +3,20 @@ package com.example.thodlydugue.kizinlakayapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
 
 /**
  * Created by Jerry on 8/18/2017.
  */
 
 public class ContentSearchActivity extends AppCompatActivity {
+
+    EditText etQuery;
+    GridView gvResults;
+    Button btnSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,4 +27,36 @@ public class ContentSearchActivity extends AppCompatActivity {
 
     }
 
+
+    public void onRecetteSearch(View view) {
+       /* String query = etQuery.getText().toString();
+        //Toast.makeText(this, "Searching for" + query, Toast.LENGTH_SHORT).show();
+        AsyncHttpClient client = new AsyncHttpClient();
+        String url = "http://api.nytimes.com/svc/search/v2/articlesearch.json";
+
+        RequestParams params = new RequestParams();
+        params.put("api-key", "198bc0cf37d3483692645939c3632343");
+        params.put("page", 0);
+        params.put("q", query);
+        client.get(url,params,new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                //super.onSuccess(statusCode, headers, response);
+                //Log.d("DEBUG", response.toString());
+                //System.out.println("Debug");
+                JSONArray articleJsonResults = null;
+                try{
+                    articleJsonResults = response.getJSONObject("response").getJSONArray("docs");
+
+                    adapter.addAll(Article.fromJSONArray(articleJsonResults));
+
+                    //Log.d("DEBUG", response.toString());
+
+                }catch(JSONException e){
+                    e.printStackTrace();
+
+                }
+            }
+        });*/
+    }
 }
