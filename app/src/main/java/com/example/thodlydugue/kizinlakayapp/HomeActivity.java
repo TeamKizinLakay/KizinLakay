59582eb19b7fb5cac229847c6dbf368bf34022eb
Thodly <thodlydugue@gmail.com>
2017-08-21 12:51:49 -0500

    Connectingallactivitytogether (#15)
    * changing some connection between the activities
    
    * changing some connection between the activities

--- app/src/main/java/com/example/thodlydugue/kizinlakayapp/HomeActivity.java
+++ app/src/main/java/com/example/thodlydugue/kizinlakayapp/HomeActivity.java
@@ -1,12 +1,17 @@
 package com.example.thodlydugue.kizinlakayapp;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class HomeActivity extends AppCompatActivity {
-//private Button bt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,24 +26,5 @@ public class HomeActivity extends AppCompatActivity {
             }
         }, secondsDelayed * 4000);
 
-
-      //  bt = (Button) findViewById(R.id.btnStart);
-        // bt.setBackgroundColor(Color.BLUE);
-       // bt.setOnClickListener(new View.OnClickListener() {
-         //   @Override
-            //public void onClick(View v) {
-
-               // Intent intent= new Intent(HomeActivity.this,MenuActivity.class);
-               // intent.putExtra("movie", movie);
-                //intent.putExtra("item",adapter.getItem(position));
-                //startActivity(intent);
-
-
-
-        //    }
-       // });
-
-
             }
-
 }
