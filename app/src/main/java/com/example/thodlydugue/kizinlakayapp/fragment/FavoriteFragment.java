652675fa4c9eaa3b4d22dd438fe25ca99761090b
Jimmy Joseph <dioucyalewemenn@yahoo.fr>
2017-09-03 09:54:47 -0700

    adding logout

--- app/src/main/java/com/example/thodlydugue/kizinlakayapp/fragment/FavoriteFragment.java
+++ app/src/main/java/com/example/thodlydugue/kizinlakayapp/fragment/FavoriteFragment.java
@@ -6,7 +6,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.backendless.Backendless;
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.exceptions.BackendlessFault;
 import com.example.thodlydugue.kizinlakayapp.R;
 
 /**
@@ -24,7 +29,29 @@ public class FavoriteFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite, container, false);
+        View view = inflater.inflate(R.layout.fragment_favorite, container, false);
+        Button btnLogOut = (Button) view.findViewById(R.id.btnLogOut);
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Backendless.UserService.logout(new AsyncCallback<Void>() {
+                    @Override
+                    public void handleResponse(Void response) {
+                        Toast.makeText(getActivity(), "You logged out", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void handleFault(BackendlessFault fault) {
+                        Toast.makeText(getActivity(), "Failed to log out", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+            }
+
+        });
+        return view;
+
     }
 
 }
