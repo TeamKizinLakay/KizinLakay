84531b9216ef6ea2521ce9b87cbb83c002db10b6
Jules Frantz Stephane Loubeau <stelou29@gmail.com>
2017-09-06 12:22:01 -0400

    splashScreen
    Splash + listVIEW

--- app/src/main/java/com/example/thodlydugue/kizinlakayapp/HomeActivity.java
+++ app/src/main/java/com/example/thodlydugue/kizinlakayapp/HomeActivity.java
@@ -1,25 +1,52 @@
 package com.example.thodlydugue.kizinlakayapp;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-
+import android.view.View;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
 
 
 public class HomeActivity extends AppCompatActivity {
 
 
+    View viewProgress;
+    AnimationSet animationSet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+
+        viewProgress = findViewById(R.id.view_progress);
+        int viewWidth = viewProgress.getWidth();
+
+        TranslateAnimation move = new TranslateAnimation(-(getScreenWidth() / 2) + viewWidth / 2, (getScreenWidth() / 2) + viewWidth / 2 + viewWidth, 0, 0);
+        move.setDuration(1000);
+        TranslateAnimation move1 = new TranslateAnimation(-viewWidth, 0, 0, 0);
+        move1.setDuration(500);
+        ScaleAnimation laftOut = new ScaleAnimation(0, 1, 1, 1);
+        laftOut.setDuration(500);
+
+        animationSet = new AnimationSet(true);
+        animationSet.addAnimation(move);
+        animationSet.addAnimation(move1);
+        animationSet.addAnimation(laftOut);
+        animationSet.addAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide));
+
+        startAnimation();
+
 //Set Splash Effect to main activity
         int secondsDelayed = 1;
         new Handler().postDelayed(new Runnable() {
             public void run() {
-                startActivity(new Intent(HomeActivity.this, MenuActivity.class));
+                Intent intent= new Intent(HomeActivity.this,MenuActivity.class);
+                startActivity(intent);
                 finish();
             }
         }, secondsDelayed * 3000);
@@ -45,7 +72,26 @@ public class HomeActivity extends AppCompatActivity {
 
             }
 
+    private void startAnimation() {
 
+        viewProgress.startAnimation(animationSet);
+        new android.os.Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startAnimation();
 
             }
+        }, 1000);
+    }
+
+
+
+    public static int getScreenWidth() {
+        return Resources.getSystem().getDisplayMetrics().widthPixels;
+    }
+
+
+
+
+}
 
