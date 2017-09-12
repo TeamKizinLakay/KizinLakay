e7b747238be5452ede85db5624a0bf50888db6c9
ThodlyDugue <thodlydugue@gmail.com>
2017-09-12 09:38:01 -0400

    backbutton-event

--- app/src/main/java/com/example/thodlydugue/kizinlakayapp/Event_Activity.java
+++ app/src/main/java/com/example/thodlydugue/kizinlakayapp/Event_Activity.java
@@ -1,14 +1,19 @@
 package com.example.thodlydugue.kizinlakayapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 /**
@@ -19,6 +24,7 @@ public class Event_Activity extends AppCompatActivity {
 
 
     String currentUrl;
+    ImageButton btnback;
 
     WebView webvalue;
     @Override
@@ -29,7 +35,6 @@ public class Event_Activity extends AppCompatActivity {
         setToolbar();
 
         if (getSupportActionBar() != null)
-            // button option
         {
            // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setTitle("Categorie Viandes")
@@ -51,7 +56,15 @@ public class Event_Activity extends AppCompatActivity {
         //webvalue.loadUrl("https://www.instagram.com/kinamhotel/");
 
         webvalue.loadUrl("https://web.facebook.com/maisonmenagere/");
+        btnback = (ImageButton)findViewById(R.id.btnBack);
+        btnback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                Intent intent = new Intent(Event_Activity.this, MenuActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
